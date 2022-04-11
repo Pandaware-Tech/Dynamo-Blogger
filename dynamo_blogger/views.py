20ec7cf9651e6f8d186faec277ac16b4111d4971
Israel Abraham <israelvictory87@gmail.com>
2022-04-11 22:16:37 +0100

    Created home__page view

--- dynamo_blogger/views.py
+++ dynamo_blogger/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+
+def home__page(request:HttpRequest) -> HttpResponse:
+    
+    context = {}
+    return render(request, "blog/index.html", context)
