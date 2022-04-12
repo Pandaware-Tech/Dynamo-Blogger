0294e6245baa99df2461d7e7746714d1866efef1
Israel Abraham <israelvictory87@gmail.com>
2022-04-12 19:53:27 +0100

    Added ckeditor to INSTALLED_APPS

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'dynamo_blogger.apps.DynamoBloggerConfig'
+    'dynamo_blogger.apps.DynamoBloggerConfig',
+    'ckeditor'
 ]
 
 MIDDLEWARE = [
