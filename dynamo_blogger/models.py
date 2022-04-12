5095d5ae9a701e92be931691509313258d7381da
Israel Abraham <israelvictory87@gmail.com>
2022-04-12 19:50:20 +0100

    Created Category, Post and Comment model

--- dynamo_blogger/models.py
+++ dynamo_blogger/models.py
@@ -1,3 +1,75 @@
+from django.utils.text import slugify
 from django.db import models
+from ckeditor.fields import RichTextField
+from django.contrib.auth import get_user_model
+from dynamo_blogger.helpers.timestamps import TimeStampedModel
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Category(TimeStampedModel):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    color_number = models.IntegerField(default=1)
+    
+    class Meta:
+        verbose_name_plural = "Blog Categories"
+        db_table = "blog_categories"
+    
+    def save(self, *args, **kwargs):
+        
+        if not self.slug:
+            slug = slugify(self.title)
+            super(Category, self).save(*args, **kwargs)
+        super(Category, self).save(*args, **kwargs)
+        
+        
+
+class Post(TimeStampedModel):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    description = RichTextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    class Meta:
+        verbose_name_plural = "Blog Posts"
+        db_table = "blog_posts"
+    
+    def save(self, *args, **kwargs):
+        
+        if not self.slug:
+            slug = slugify(self.title)
+            super(Post, self).save(*args, **kwargs)
+        super(Post, self).save(*args, **kwargs)
+        
+    
+    def get_number_of_comments(self):
+        """
+        It returns the number of comments associated with a post
+        :return: The number of comments for a given post.
+        """
+        comment_count = Comment.objects.filter(blog_post_id=self.id).count()
+        return comment_count
+        
+        
+        
+class Comment(TimeStampedModel):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    website = models.URLField(max_length=255)
+    message = models.TextField(max_length=600)
+    blog_post = models.ForeignKey(Post, blank=True, null=True, on_delete=models.CASCADE)
+    reply_to = models.ForeignKey("self", blank=True, null=True, on_delete=models.SET_NULL)
+    
+    class Meta:
+        verbose_name_plural = "Blog Comments"
+        db_table = "blog_comments"
+        
+    def __str__(self) -> str:
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        email = self.email.lower()
+        super(Comment, self).save(*args, **kwargs)
+    
