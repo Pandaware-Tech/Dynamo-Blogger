dc9d306f9ef6797a61471dc1a91721a1a8d6d4ed
Israel Abraham <israelvictory87@gmail.com>
2022-04-15 21:22:14 +0100

    Added social links as context variable to base template

--- dynamo_blogger/views.py
+++ dynamo_blogger/views.py
@@ -6,7 +6,11 @@ from django.http import HttpRequest, HttpResponse
 def home__page(request:HttpRequest) -> HttpResponse:
     
     context = {
-        "site__name": settings.DYNAMO_BLOGGER['site_name']
+        "site__name": settings.DYNAMO_BLOGGER['site_name'],
+        "facebook": settings.DYNAMO_BLOGGER['facebook'],
+        "twitter": settings.DYNAMO_BLOGGER['twitter'],
+        "linkedin": settings.DYNAMO_BLOGGER['linkedin'],
+        "instagram": settings.DYNAMO_BLOGGER['instagram'],
     }
     return render(request, "blog/index.html", context)
 
