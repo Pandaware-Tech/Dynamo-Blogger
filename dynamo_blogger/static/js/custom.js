43c55af9ae7176bb4b610f8e6e7e54687c8c1fdd
Israel Abraham <israelvictory87@gmail.com>
2022-04-17 19:06:42 +0100

    Update aside nav

--- dynamo_blogger/static/js/custom.js
+++ dynamo_blogger/static/js/custom.js
@@ -1,14 +1,22 @@
 let toggleBtn = document.getElementById("toggelBtn");
+let closeAsideNavBtn = document.querySelector(".nav-aside-close");
 let navAside = document.getElementById("nav-aside");
 
 
 // Click event listener
 toggleBtn.addEventListener("click", toggleNavAside);
-
+closeAsideNavBtn.addEventListener("click", closeNavAside);
 
 // Function to toggle nav aside
 function toggleNavAside() {
 
     // Toggle aside nav
     navAside.classList.toggle("top_350");
+}
+
+// Function to close nav aside
+function closeNavAside() {
+
+    // RHide aside nav
+    navAside.classList.remove("top_350");
 }
