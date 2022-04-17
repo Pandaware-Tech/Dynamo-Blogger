7b7306a81a73f416fb9af91609cd365548c65cce
Israel Abraham <israelvictory87@gmail.com>
2022-04-17 12:07:13 +0100

    Created PostAuthorAdmin

--- dynamo_blogger/admin.py
+++ dynamo_blogger/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from dynamo_blogger.models import Category, Post, Comment
+from dynamo_blogger.models import Category, Post, Comment, PostAuthor
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(PostAuthor)
+class PostAuthorAdmin(admin.ModelAdmin):
     pass
 
 
